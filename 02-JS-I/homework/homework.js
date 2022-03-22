64f29754788feabf1798d5c274d36f777a3760fd
AnguilaZ7 <soterasjose22@gmail.com>
2022-03-22 02:16:51 -0300

    finish homework 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'AnguilaZ7';
 const nuevoNum = 77;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
